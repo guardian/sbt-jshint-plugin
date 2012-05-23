a8e2f5fd00f86481cd9437b1ab9e64b8496bb5aa
steppenwells <steppenwells@gmail.com>
2012-05-23 09:11:05 +0100

    allowes options file to be configured

--- src/main/scala/com/gu/SbtJshintPlugin.scala
+++ src/main/scala/com/gu/SbtJshintPlugin.scala
@@ -2,23 +2,24 @@ package com.gu
 
 import sbt._
 import Keys._
-import java.io.InputStreamReader
 import io.Source
 import org.mozilla.javascript.{ScriptableObject, ContextFactory, Context, Function => JsFunction}
 import org.mozilla.javascript.tools.shell.{Global, Main}
+import java.io.{FileReader, InputStreamReader}
 
 
 object SbtJshintPlugin extends Plugin {
 
   val jshintConf = config("jshint") hide
 
-  //lazy val distPath = SettingKey[File]("jshint-options", "Path to file containing the jshint options")
+  lazy val jshintOptions = SettingKey[Seq[File]]("jshintOptions", "Path to .js file containing the jshint options, must define a JSLINT_OPTIONS object")
   lazy val jshintFiles = SettingKey[Seq[PathFinder]]("jshintFiles", "the files to run jshint against")
   lazy val jshintMercy = SettingKey[Int]("jshintMercy", "the number of errors allowed before the jshint task fails")
   lazy val jshint = TaskKey[Unit]("jshint", "Run jshint code analysis")
 
-  def jshintTask = (jshintFiles, jshintMercy, streams) map { (optFiles, mercy, s) =>
-    optFiles.foreach { files =>
+  def jshintTask = (jshintOptions, jshintFiles, jshintMercy, streams) map { (optFiles, testFiles, mercy, s) =>
+
+    testFiles.foreach { files =>
       s.log.info("running jshint...")
 
       val jscontext =  new ContextFactory().enterContext()
@@ -27,7 +28,14 @@ object SbtJshintPlugin extends Plugin {
       scope.init(jscontext)
 
       jscontext.evaluateReader(scope, bundledScript("jshint.js"), "jshint.js", 1, null)
-      jscontext.evaluateReader(scope, bundledScript("defaultOptions.js"), "defaultOptions.js", 1, null)
+
+      val optionsFile = optFiles.headOption
+
+      optionsFile match {
+        case Some(f) => jscontext.evaluateString(scope, readFile(f), f.getName, 1, null)
+        case _ => jscontext.evaluateReader(scope, bundledScript("defaultOptions.js"), "defaultOptions.js", 1, null)
+      }
+
       jscontext.evaluateReader(scope, bundledScript("getErrors.js"), "getErrors.js", 1, null)
 
       val errorCount = files.get.map { jsfile =>
@@ -39,6 +47,7 @@ object SbtJshintPlugin extends Plugin {
       if (errorCount > mercy) throw new JshintFailedException(errorCount.toInt)
 
     }
+
   }
 
   def bundledScript(fileName: String) = {
@@ -60,7 +69,8 @@ object SbtJshintPlugin extends Plugin {
   val jshintSettings: Seq[Project.Setting[_]] = Seq(
     jshint <<= jshintTask,
     jshintFiles := Seq(),
-    jshintMercy := 0
+    jshintMercy := 0,
+    jshintOptions := Seq()
   )
 }
 
